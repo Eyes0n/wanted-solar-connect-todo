79142ba32af22770085a88fe7ffd913b6b76872e
hyunchanpark <eye3570@gmail.com>
2021-08-19 18:35:42 +0900

    Fix : 로컬스토리지에서 불러오는 data가 null인 경우에 대한 초기값 설정

--- src/components/todo/TodoService.tsx
+++ src/components/todo/TodoService.tsx
@@ -36,20 +36,25 @@ export const useTodo = (): any => {
   };
 
   const createTodo = (todo: Itodo) => {
-    const nextId = todoState.length + 1;
+    const nextId = nextIdState + 1;
+
     setTodoState((prevState) =>
       prevState.concat({
         ...todo,
         id: nextId,
       })
     );
+    incrementNextId();
+    console.log('nextIdState :>> ', nextIdState);
   };
 
   const loadData = () => {
-    let data = localStorage.getItem('todos');
-    if (data === undefined) data = '';
-    initialTodos = JSON.parse(data!);
+    const data = localStorage.getItem('todos') || '[]';
+    initialTodos = JSON.parse(data);
+    console.log('initialTodos :>> ', initialTodos);
     if (initialTodos && initialTodos.length >= 1) {
+      // TODO: todos 갯수가 0부터 시작하는거면 괜찬 but 여러개가 있는 경우면 문제가됨
+      // TODO: 여러개로 load되는 부분 생각하여 작성
       incrementNextId();
     }
     setTodoState(initialTodos);
