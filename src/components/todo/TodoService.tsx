1c37721e4a2c5e964bc48005bfed85515e908500
hyunchanpark <eye3570@gmail.com>
2021-08-19 18:42:36 +0900

    Fix : nextIdState 초기화 현상 수정

--- src/components/todo/TodoService.tsx
+++ src/components/todo/TodoService.tsx
@@ -8,10 +8,10 @@ export type Itodo = {
 };
 
 let initialTodos: Itodo[] = [];
+let nextIdState = 0;
 
 export const useTodo = (): any => {
   const [todoState, setTodoState] = useState(initialTodos);
-  let nextIdState = 0;
 
   useEffect(() => {
     loadData();
@@ -45,16 +45,14 @@ export const useTodo = (): any => {
       })
     );
     incrementNextId();
-    console.log('nextIdState :>> ', nextIdState);
   };
 
   const loadData = () => {
     const data = localStorage.getItem('todos') || '[]';
     initialTodos = JSON.parse(data);
-    console.log('initialTodos :>> ', initialTodos);
+
     if (initialTodos && initialTodos.length >= 1) {
-      // TODO: todos 갯수가 0부터 시작하는거면 괜찬 but 여러개가 있는 경우면 문제가됨
-      // TODO: 여러개로 load되는 부분 생각하여 작성
+      nextIdState = initialTodos[initialTodos.length - 1].id;
       incrementNextId();
     }
     setTodoState(initialTodos);
